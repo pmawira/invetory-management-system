3763c8a19b1654052874ec595f1bdfd54e448c04
pmawira <pmawira@gmail.com>
2025-03-07 18:25:10 +0300

    -Added product repository for data access and manupulation

---
+++ src/Inventory.Management.System.Logic/Features/Products/Database/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Inventory.Management.System.Logic.DataAccess;
+using Inventory.Management.System.Logic.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Management.System.Logic.Features.Products.Database
+{
+    public interface IProductRepository:IBaseGenericRepository<Product>
+    {
+        Product GetProductByName(string name);
+    }
+}
---
+++ src/Inventory.Management.System.Logic/Features/Products/Database/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Inventory.Management.System.Logic.DataAccess;
+using Inventory.Management.System.Logic.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Management.System.Logic.Features.Products.Database
+{
+    public class ProductRepository : BaseGenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(IUnitOfWork uow) : base(uow)
+        {
+        }
+        public Product? GetProductByName(string name)
+        {
+            return Uow.Context.Set<Product>()
+                .FirstOrDefault(p => p.Name == name);
+        }
+    }
+}
