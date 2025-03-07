d8afa12f00b0f2f7bc87fa67982734c93a93a251
pmawira <pmawira@gmail.com>
2025-03-07 18:27:27 +0300

    # added commands and dto

---
+++ src/Inventory.Management.System.Logic/Features/Products/Commands/ProductCreateCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Management.System.Logic.Features.Products.Commands
+{
+    public class ProductCreateCommand
+    {
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
