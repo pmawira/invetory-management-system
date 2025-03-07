f1e78f0f875b58bd198da324313e0624d7f96853
pmawira <pmawira@gmail.com>
2025-03-07 18:28:11 +0300

    #added ProductMappingProfile for mapping

---
+++ src/Inventory.Management.System.Logic/Features/Products/ProductMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Inventory.Management.System.Logic.Database.Models;
+using Inventory.Management.System.Logic.Features.Products.Commands;
+using Inventory.Management.System.Logic.Features.Products.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Inventory.Management.System.Logic.Features.Products
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<ProductCreateRequest, Product>()
+             .ForMember(dest => dest.Id, opt => opt.Ignore()) // Ignore ID (DB-generated)
+             .ForMember(dest => dest.DateCreated, opt => opt.Ignore()) // System-managed
+             .ForMember(dest => dest.DateModified, opt => opt.Ignore()) // System-managed
+             .ForMember(dest => dest.Category, opt => opt.Ignore()); // Navigation property
+            CreateMap<ProductCreateRequest, ProductCreateCommand>();
+        }
+    }
+}
